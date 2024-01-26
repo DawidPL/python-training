9dba3ed3b7f868310f686622c10461328ac7e932
Dawid <dawid.hrynkiewicz@internet-plus.pl>
2024-01-26 08:31:57 +0100

    Task 10

--- part1-basic_exercises.py
+++ part1-basic_exercises.py
@@ -180,3 +180,22 @@ def palidrome(number):
 
 task_nine = palidrome(151)
 print(task_nine)
+
+'''
+Exercise 10: Create a new list from two list using the following condition
+
+Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+'''
+
+def new_list(list1, list2):
+    new_list = []
+    for num in list1:
+        if num % 2 != 0:
+             new_list.append(num)
+    for num in list2:
+        if num % 2 == 0:
+            new_list.append(num)
+    return new_list
+
+task_ten = new_list([10, 20, 25, 30, 35], [40, 45, 60, 75, 90])
+print (task_ten)
