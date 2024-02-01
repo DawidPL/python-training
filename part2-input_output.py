99b3159382d32262cc8cf37c5a9d020f445c8ed9
Dawid <dawid.hrynkiewicz@internet-plus.pl>
2024-02-01 01:57:36 +0100

    Task 2 and 3

--- part2-input_output.py
+++ part2-input_output.py
@@ -11,3 +11,24 @@ def input_multiplication():
 
 task_one = input_multiplication()
 print(task_one)
+
+'''
+Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+'''
+
+def string_display(string_one, string_two, string_three):
+    print(string_one, string_two, string_three, sep='**')
+
+task_two = string_display('Name', 'Is', 'David')
+print(task_two)
+
+'''
+Exercise 3: Convert Decimal number to octal using print() output formatting
+'''
+
+def decimal_converter(num):
+    print('%o' % num) 
+
+task_three = decimal_converter(8)
+print(task_three)
